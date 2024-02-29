f86751b7cde13dcde068ffa090354c7d8aaf68c6
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-02-29 14:58:00 -0500

    Object1

---
+++ object1/src/main/java/object1/Car.java
@@ -0,0 +1,38 @@
+package object1;
+
+public class Car {
+    private String make;
+    private String model;
+    private int year;
+
+
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+
+
+    }
--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -5,6 +5,28 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String... args) {
-        Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
-}
+        Logger logger = Logger.getLogger(Main.class.getName());
+        logger.info("This is a module-using Hello World!");
+
+        Person person = new Person("Jane", 23, 180);
+        System.out.println("Name: " + person.name);
+        System.out.println("Age: " + person.age);
+        System.out.println("Height: " + person.height);
+
+        Car car = new Car("Honda", "CRV", 2015);
+        System.out.println("Make: " + car.getMake());
+        System.out.println("Model: " + car.getModel());
+        System.out.println("Year: " + car.getYear());
+
+        Chair chair = new Chair("Leather", "Blue", 2);
+        System.out.println("Material: " + chair.getColor());
+        System.out.println("Color: " + chair.getMaterial());
+        System.out.println("Legs: " +chair.getLegs());
+
+        Dog dog = new Dog("Donut", 1, "Golden retriever");
+        System.out.println("Name: "+ dog.getName());
+        System.out.println("Age: "+ dog.getBreed());
+        System.out.println("Breed: "+dog.getAge());
+    }
+    }
+
